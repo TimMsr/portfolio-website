72ba41e19ec28ea8cb67bbf92d60d0a5a5d3d518
Tim M <153383959+TimMsr@users.noreply.github.com>
2024-12-23 19:06:31 +0000

    Added Copy Functionality

--- script.js
+++ script.js
@@ -3,5 +3,22 @@ const navLinks = document.querySelector('.nav-links');
 
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
-
 }
+
+const sendButton = document.getElementById('send-btn');
+
+sendButton.addEventListener('click', function() {
+    const tempInput = document.createElement('textarea');
+    tempInput.value = 'tim.maeser2@gmail.com'; 
+    document.body.appendChild(tempInput); 
+
+
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); 
+
+    document.execCommand('copy');
+
+    document.body.removeChild(tempInput);
+
+    alert('Email copied to clipboard!');
+});
